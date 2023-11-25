caecac18c06208ffb43729a6ad6f83268a99c59a
archil <sitsocker@GMAIL.COM>
2023-11-25 22:32:32 +0400

    changed

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 const WorkItems = ({ item }) => {
   return (
     <div className='work__card' key={item.id}>
       <img src={item.image} alt='Img' className='work__img' />
       <h3 className='work__title'>{item.title}</h3>
-      <a href='#' className='work__button'>
+      <a
+        href='https://github.com/achikoooo/CreateX'
+        target='_blank'
+        className='work__button'
+        rel='noreferrer'
+      >
         Demo
         <i class='uil uil-arrow-right work__button-icon'></i>
       </a>
     </div>
-  );
-};
+  )
+}
 
-export default WorkItems;
+export default WorkItems
